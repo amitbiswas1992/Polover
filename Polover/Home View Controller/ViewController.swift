fd82879b256f1eb2600621e629cafd389cc10506
Amit Biswas <biswas1992@hotmail.com>
2018-07-27 12:10:13 -0400

    Sign Up and Log In button Implemented

--- Polover/Home View Controller/ViewController.swift
+++ Polover/Home View Controller/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField:UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var signUPButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var driverLabel: UILabel!
+    
+    var signUpMood = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signUPTapped(_ sender: UIButton) {
+        
+        
     }
-
-
+    
+    @IBAction func logInTapped(_ sender: UIButton) {
+        if signUpMood{
+            signUPButton.setTitle("Log In", for: .normal)
+            logInButton.setTitle("Switch To Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMood = false
+            
+        }
+        else {
+            signUPButton.setTitle("Sign Up", for: .normal)
+            logInButton.setTitle("Switch To Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMood = true
+        }
+        
+    }
+    
 }
 
